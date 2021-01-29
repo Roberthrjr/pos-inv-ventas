5950a6f08a0741f554ebf03b0fd617fdde724421
Roberth Rios <tauro_roberth00@hotmail.com>
2021-01-29 09:30:11 -0500

    Se actualizo la plantilla.php

--- view/modules/cabezote.php
+++ view/modules/cabezote.php
@@ -2,8 +2,13 @@
     <!-- LOGOTIPO -->
     <a href="" class="logo">
         <!-- LOGO MINI -->
-        <span class="logo-mini"></span>
+        <span class="logo-mini">
+            <img src="view/img/plantilla/icono-blanco.png" class="img-responsive" style="padding:10px">
+        </span>
         <!-- LOGO NORMAL -->
-        <span></span>
+        <span class="logo-mini">
+            <img src="view/img/plantilla/logo-blanco-lineal.png" class="img-responsive" style="padding:10px 0px">
+        </span>
     </a>
+    <!-- BARRA DE NAVEGACION -->
 </header>
