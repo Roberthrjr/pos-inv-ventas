21a4e22f3f0f12ff98fc41119bbf4bd67e60b404
Roberth Rios <tauroroberth00@gmail.com>
2021-03-07 19:46:57 -0500

    Se terminó el modulo de ventas y se culmino correctamente el proceso de registro de ventas

--- view/modules/footer.php
+++ view/modules/footer.php
@@ -1,6 +1,6 @@
 <footer class="main-footer">
     <div class="float-right d-none d-sm-block">
-      <b>Version</b> 3.1.0-rc
+      <b>Version</b> 1.0
     </div>
-    <strong>Copyright &copy; 2014-2020 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
+    <strong>Copyright &copy; 2021 <a target="_blank" href="https://github.com/Roberthrjr/pos-inv-ventas">La Gran Familia R&S</a>.</strong> Todos los derechos reservados.
 </footer>
